a3794843088ba699f54286addb5810785be8457e
Mucciz <kenmucciz8@gmail.com>
2022-07-18 12:33:55 +0300

    Add error handling

--- translate_lang.py
+++ translate_lang.py
@@ -1,17 +1,20 @@
-import os
 import openai
+from decouple import config
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = config('OPENAI_API_KEY')
 
 def get_translation(text, lang):
     response = openai.Completion.create(
-    model="text-davinci-002",
-    prompt= f"Translate this into {lang}\n\n{text}",
-    temperature=0.3,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+        model="text-davinci-002",
+        prompt= f"Translate this into {lang}\n\n{text}",
+        temperature=0.3,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
 
-    return response.choices[0].text
+    try:
+        return response.choices[0].text
+    except:
+        return "Houston, we have a problem"
