164166a63d521b3a19247323dfae7cb6673e1140
Mucciz <kenmucciz8@gmail.com>
2022-07-18 15:06:58 +0300

    Add python decouple

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import os
-
+from decouple import config
 from flask import Flask, render_template, request, redirect, url_for
+from translate_lang import get_translation
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    if request.method == 'POST':
+        text = request.form['aiPrompt']
+        lang = request.form['language']
+        answer = get_translation(text, lang)
 
     return render_template('index.html', **locals())
